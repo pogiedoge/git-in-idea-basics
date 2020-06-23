bb0c9dd3c56fb606f5aa5ad5125c52ed1f330489
Csabi | BackendDev | Wanari <csibcsab@wanari.com>
2020-06-23 10:18:06 +0200

    Add printAll to Main

--- src/main/scala/com/wanari/git_in_idea_basics/Main.scala
+++ src/main/scala/com/wanari/git_in_idea_basics/Main.scala
@@ -3,14 +3,19 @@ package com.wanari.git_in_idea_basics
 import com.wanari.git_in_idea_basics.maths.Complex
 
 object Main extends App {
+  def printAll(c1: Complex, c2: Complex): Unit = {
+    println(c1 + c2)
+    println(c2 - c1)
+    println(c1.r)
+    println(c1.angle)
+    println(c1.inv)
+    println(c1.toTrigonometric)
+    println(c1.toExponential)
+    println(c1 ^ 2)
+  }
+
   val a = Complex(1, 2)
   val b = Complex(2, 1)
-  println(a + b)
-  println(b - a)
-  println(a.r)
-  println(a.angle)
-  println(a.inv)
-  println(a.toTrigonometric)
-  println(a.toExponential)
-  println(a ^ 2)
+
+  printAll(a, b)
 }
