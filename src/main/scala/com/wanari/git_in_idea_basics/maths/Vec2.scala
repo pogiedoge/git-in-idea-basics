666fdc2ee1c0e06147551f98f1b480dc3f8079d9
Csabi | BackendDev | Wanari <csibcsab@wanari.com>
2020-06-23 13:52:36 +0200

    Rename Vec2D to Vec2

---
+++ src/main/scala/com/wanari/git_in_idea_basics/maths/Vec2.scala
@@ -0,0 +1,15 @@
+package com.wanari.git_in_idea_basics.maths
+
+case class Vec2(start: Point2D, end: Point2D) { left: Vec2 =>
+  def +(right: Vec2): Vec2 = {
+    Vec2(left.start + right.start, left.end + right.end)
+  }
+
+  def -(right: Vec2): Vec2 = {
+    Vec2(left.start - right.start, left.end - right.end)
+  }
+
+  def *(scalar: Double): Vec2 = {
+    Vec2(start * scalar, end * scalar)
+  }
+}
