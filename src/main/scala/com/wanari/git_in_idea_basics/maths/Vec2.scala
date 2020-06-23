3d2b0613756932df49fc08a23b9214abf4b435d3
Csabi | BackendDev | Wanari <csibcsab@wanari.com>
2020-06-23 14:59:39 +0200

    Add debug printlns to Vec2

--- src/main/scala/com/wanari/git_in_idea_basics/maths/Vec2.scala
+++ src/main/scala/com/wanari/git_in_idea_basics/maths/Vec2.scala
@@ -2,12 +2,12 @@ package com.wanari.git_in_idea_basics.maths
 
 case class Vec2(start: Point2, end: Point2) { left: Vec2 =>
   def +(right: Vec2): Vec2 = {
+    println(s"(${left.start}+${right.start}; ${left.end}+${right.end})")
     println(s"Adding two two-dimensional vectors. Vec2 1: $left, Vec2 2: $right.")
     Vec2(left.start + right.start, left.end + right.end)
   }
 
   def -(right: Vec2): Vec2 = {
-    println(s"Subtracting two two-dimensional vectors. Vec2 1: $left, Vec2 2: $right.")
     Vec2(left.start - right.start, left.end - right.end)
   }
 
