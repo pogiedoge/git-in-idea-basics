d28497e8b740ad4b5189f65b38ea0558226a59e0
pogiedoge <62297078+pogiedoge@users.noreply.github.com>
2020-06-23 14:48:52 +0200

    Add debug printlns

--- src/main/scala/com/wanari/git_in_idea_basics/maths/Point2.scala
+++ src/main/scala/com/wanari/git_in_idea_basics/maths/Point2.scala
@@ -2,17 +2,17 @@ package com.wanari.git_in_idea_basics.maths
 
 case class Point2(x: Double, y: Double) { left: Point2 =>
   def +(right: Point2): Point2 = {
-    println(s"Adding two points. Point 1: $left, Point 2: $right")
+    println(s"(${left.x}+${right.x}, ${left.y}+${right.y})")
     Point2(left.x + right.x, left.y + right.y)
   }
 
   def -(right: Point2): Point2 = {
-    println(s"Subtracting two points. Point 1: $left, Point 2: $right")
+    println(s"(${left.x}-${right.x}, ${left.y}-${right.y})")
     Point2(left.x - right.x, left.y + right.y)
   }
 
   def *(scalar: Double): Point2 = {
-    println(s"Scalar multiplication of a point. Scalar: $scalar, point: $left")
+    println(s"(${left.x}*scalar, ${left.y}*scalar)")
     Point2(scalar * x, scalar * y)
   }
 }
