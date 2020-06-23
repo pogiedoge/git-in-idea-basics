17b901e273f8f989b39daa42e2c0644e2dd3cc00
pogiedoge <62297078+pogiedoge@users.noreply.github.com>
2020-06-23 13:58:29 +0200

    Add debug printlns to Point2D

--- src/main/scala/com/wanari/git_in_idea_basics/maths/Point2D.scala
+++ src/main/scala/com/wanari/git_in_idea_basics/maths/Point2D.scala
@@ -2,14 +2,17 @@ package com.wanari.git_in_idea_basics.maths
 
 case class Point2D(x: Double, y: Double) { left: Point2D =>
   def +(right: Point2D): Point2D = {
+    println(s"(${left.x}+${right.x}, ${left.y}+${right.y})")
     Point2D(left.x + right.x, left.y + right.y)
   }
 
   def -(right: Point2D): Point2D = {
+    println(s"(${left.x}-${right.x}, ${left.y}-${right.y})")
     Point2D(left.x - right.x, left.y + right.y)
   }
 
   def *(scalar: Double): Point2D = {
+    println(s"(${left.x}*scalar, ${left.y}*scalar)")
     Point2D(scalar * x, scalar * y)
   }
 }
