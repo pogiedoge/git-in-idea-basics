0cc74bd383a4639105d52a009b39e1cdd28dda6f
Csabi | BackendDev | Wanari <csibcsab@wanari.com>
2020-06-30 16:27:55 +0200

    Add debug printlns to Point3

--- src/main/scala/com/wanari/git_in_idea_basics/maths/Point3.scala
+++ src/main/scala/com/wanari/git_in_idea_basics/maths/Point3.scala
@@ -1,15 +1,20 @@
 package com.wanari.git_in_idea_basics.maths
 
 case class Point3(x: Double, y: Double, z: Double) { left: Point3 =>
+  println(s"Point3 created P($x; $y; $z).")
+
   def +(right: Point3): Point3 = {
+    println(s"Adding two three-dimensional points. Left: $left, right: $right.")
     Point3(left.x + right.x, left.y + right.y, left.z + right.z)
   }
 
   def -(right: Point3): Point3 = {
+    println(s"Subtracting two three-dimensional points. Left: $left, right: $right.")
     Point3(left.x - right.x, left.y + right.y, left.z - right.z)
   }
 
   def *(scalar: Double): Point3 = {
+    println(s"Scalar multiplication of a three-dimensional point. Scalar: $scalar, point: $left.")
     Point3(scalar * x, scalar * y, scalar * z)
   }
 }
